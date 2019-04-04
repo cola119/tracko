30eb759a402da3786d36a361b547bd95e5180845
kouhei ueno <k.cola119@gmail.com>
2019-04-04 11:30:23 +0900

    mapbox addLayer APIでO-map表示

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,39 +2,72 @@ import React from 'react';
 import ReactMapGL from 'react-map-gl';
 import UserPointer from '../components/UserPointer'
 import BackgroundImage from '../components/BackgroundImage'
+import {fromJS} from 'immutable';
 
-const Map = (props) => {
-
-	return (
-		<ReactMapGL
-			width='100%'
-			height='100vh'
-			visible={props.mapViewFlag}
-			{...props.viewport}
-			mapboxApiAccessToken={props.token}
-			onViewportChange={props.onViewportChange}>
-			<BackgroundImage
-				comp={props.comp}
-			/>
-			{
-				props.users.filter((user) => {
-					return props.userlist[user].class === props.selectedClass
-				}).map((user) => {
-					return (
-						<UserPointer
-							key={user}
-							user={user}
-							viewallFlag={props.viewallFlags[user]}
-							userinfo={props.userlist[user]}
-							userlocation={props.userlocations[user]}
-							sliderValue={props.sliderValue}
-							pointerRate={props.pointerRate}
-						/>
-					);
-				})
-			}
-		</ReactMapGL>
-	);
+// const Map = (props) => {
+class Map extends React.Component {
+	constructor(props) {
+		super(props);
+		this._handleMapLoaded = this._handleMapLoaded.bind(this);
+		this._mapRef = React.createRef();
+	}
+	_getMap = () => {
+		return this._mapRef.current ? this._mapRef.current.getMap() : null;
+	}
+	_handleMapLoaded = (event) => {
+		const map = this._getMap();
+		map.addLayer({
+			"id": "overlay",
+			"source": {
+				"type": "image",
+				"url": `${process.env.PUBLIC_URL}/img/${this.props.comp.image.name}`,
+				"coordinates": [
+					[139.6227259, 36.7766127],
+					[139.656034, 36.7766127],
+					[139.656034, 36.747737],
+					[139.6227259, 36.747737]
+				]
+			},
+			"type": "raster",
+		});
+	}
+	render() {
+		const props = this.props;
+		return (
+			<ReactMapGL
+				ref={this._mapRef}
+				width='100%'
+				height='100vh'
+				visible={props.mapViewFlag}
+				{...props.viewport}
+				// mapStyle={mapStyle}
+				// onLoad={this._handleMapLoaded}
+				mapboxApiAccessToken={props.token}
+				onViewportChange={props.onViewportChange}
+			>
+				<BackgroundImage
+					comp={props.comp}
+				/>
+				{
+					props.users.filter((user) => {
+						return props.userlist[user].class === props.selectedClass
+					}).map((user) => {
+						return (
+							<UserPointer
+								key={user}
+								user={user}
+								viewallFlag={props.viewallFlags[user]}
+								userinfo={props.userlist[user]}
+								userlocation={props.userlocations[user]}
+								sliderValue={props.sliderValue}
+								pointerRate={props.pointerRate}
+							/>
+						);
+					})
+				}
+			</ReactMapGL>
+		);
+	}
 };
 
 export default Map
